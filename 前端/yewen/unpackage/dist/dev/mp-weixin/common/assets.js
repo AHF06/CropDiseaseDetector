4060fde26dfcd93e6d3766567fa94eb58f448c11
AHF06 <2703817770@qq.com>
2026-04-28 14:22:27 +0800

    Delete 前端/yewen directory

--- 前端/yewen/unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const _imports_0$1 = "/static/picture.jpg";
-const _imports_0 = "/static/ai.jpg";
-exports._imports_0 = _imports_0;
-exports._imports_0$1 = _imports_0$1;
-//# sourceMappingURL=../../.sourcemap/mp-weixin/common/assets.js.map
